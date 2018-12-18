ee02176a47890ab4387675b0b9fb20c2207c4caf
xxiaojiujiu <35479120+xxiaojiujiu@users.noreply.github.com>
2018-12-18 12:42:29 +0800

    Add files via upload

---
+++ 作业6/homework6.2_AutoLayout/SixthWork_task2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SixthWork_task2
+//
+//  Created by student on 2018/12/15.
+//  Copyright © 2018年 wq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
